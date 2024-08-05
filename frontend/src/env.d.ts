7af78e670011adeff741a02d42c23420027234d2
bitfalt <75431447+bitfalt@users.noreply.github.com>
2024-08-04 23:36:19 -0600

    Add env configuration

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly XATA_APY_KEY: string
+    readonly XATA_BRANCH?: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
