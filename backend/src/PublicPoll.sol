b81e5a95ebd7c8210328ff075740b62be4bd1861
bitfalt <75431447+bitfalt@users.noreply.github.com>
2024-08-09 17:47:26 -0600

    Add nullifier implementation

--- backend/src/PublicPoll.sol
+++ backend/src/PublicPoll.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.26;
 contract PublicPoll {
     address public owner;
     mapping(string => uint) public votes;
+    mapping(bytes32 => bool) public nullifiers;
 
     event Vote(address voter, string option);
 
@@ -11,8 +12,14 @@ contract PublicPoll {
         owner = msg.sender;
     }
  
-   function castVote(string memory _option) public {
+   function castVote(string memory _option, bytes32 _nullifier) public {
+        // Check if the user hasn't voted already
+        require(!_checkNullifier(_nullifier));
         votes[_option] += 1;
         emit Vote(msg.sender, _option);
+   }
+
+   function _checkNullifier(bytes32 _nullifier) internal view returns (bool) {
+        return nullifiers[_nullifier];
    }
 }
