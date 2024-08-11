371b49f55836d5be18d9765c6c4070d8cffbdec8
bitfalt <75431447+bitfalt@users.noreply.github.com>
2024-08-11 02:13:56 -0600

    fix: remove unused dependencies

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()],
+  integrations: [react(), tailwind()],
   output: "server",
   adapter: vercel()
 });
